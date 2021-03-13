846a82f2f92273c09243fe97f01903797f22d3b3
Lewis Watson <lewiswatson55@protonmail.com>
2021-03-13 17:06:02 +0000

    Create CapitalCity Class

---
+++ src/main/java/com/napier/sem/CapitalCity.java
@@ -0,0 +1,28 @@
+package com.napier.sem;
+
+/**
+ * Represents a CapitalCity
+ */
+public class CapitalCity
+{
+    /**
+     * City name
+     */
+    public String Name;
+
+    /**
+     * Country Name
+     */
+    public String CountryCode;
+
+    /**
+     * District name
+     */
+    public String District;
+
+    /**
+     * City population
+     */
+    public int Population;
+
+}
