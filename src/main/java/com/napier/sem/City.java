2228da8df78f52bdf9d214c940d9b26b5eaf3f22
Lewis Watson <lewiswatson55@protonmail.com>
2021-04-09 17:33:17 +0100

    City Class Refactored

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -3,30 +3,125 @@ package com.napier.sem;
 /**
  * Class that represents a City.
  *
- * @author  Kristiyan Tanev
- * @version 1.0
- * @since   2021-02-23
+ * @author  Kristiyan Tanev, Lewis Watson
+ * @version 2.0
+ * @since   2021-02-13
  */
 public class City
 {
+
+    // Properties
     /**
      * City name
      */
-    public String Name;
+    private String name;
 
     /**
      * Country Name
      */
-    public String CountryCode;
+    private String countryCode;
 
     /**
      * District name
      */
-    public String District;
+    private String district;
 
     /**
      * City population
      */
-    public int Population;
+    private int population;
 
+    // Constructors
+    /**
+     * Constructor for city class
+     * @param name City Name
+     * @param countryCode City's Country Code
+     * @param district City's District
+     * @param population Population of City
+     */
+    public City(String name, String countryCode, String district, int population) {
+        this.name = name;
+        this.countryCode = countryCode;
+        this.district = district;
+        this.population = population;
+    }
+
+    /**
+     * Blank City Constructor
+     */
+    public City() {}
+
+    // Getters and Setters
+    /**
+     * Returns property name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets property name
+     * @param name City's Name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns property country code
+     */
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    /**
+     * Sets property country code
+     * @param countryCode City's Country Code
+     */
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    /**
+     * Returns property district
+     */
+    public String getDistrict() {
+        return district;
+    }
+
+    /**
+     * Sets property district
+     * @param district City's district
+     */
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    /**
+     * Returns property population
+     */
+    public int getPopulation() {
+        return population;
+    }
+
+    /**
+     * Sets property name
+     * @param population
+     */
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    /**
+     * Returns toString of City
+     */
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                '}';
+    }
 }
